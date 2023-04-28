dcee61a85d89aadaf282f1c8315028f5a9494cb0
Piotr Kasprzycki <piotr.kasprzycki123@gmail.com>
2023-04-28 19:11:57 +0200

    Capslock bug fix

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const keys = [...document.querySelectorAll(".key")];
 
 const play = function (key) {
+  key.style.boxShadow = "rgba(0, 0, 0, 0.35) 10px -10px 30px -20px inset";
+  setInterval(
+    () => (key.style.boxShadow = "rgba(0, 0, 0, 0.2) 0px 10px 15px"),
+    400
+  );
   const audio = key.querySelector("audio");
   audio.currentTime = 0;
   audio.play();
@@ -9,6 +14,10 @@ const play = function (key) {
 keys.forEach((key) => key.addEventListener("click", () => play(key)));
 
 document.addEventListener("keydown", (e) => {
-  const targetKey = keys.find((el) => el.dataset.keyboardKey === e.key);
+  const targetKey = keys.find(
+    (el) =>
+      el.dataset.keyboardKey === e.key ||
+      el.dataset.keyboardKey.toUpperCase() === e.key
+  );
   if (targetKey) play(targetKey);
 });
